728d5b4bf57cb1002f0eba13c844679c18533848
Kieran Patrick Fitzmaurice (kieranf) <kpf10@pitt.edu>
2025-07-09 21:43:36 -0400

    added SLURM script for catchment-averaged precip

---
+++ rainfall_runoff/catchment_average_precip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH -p general
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem=64g
+#SBATCH -t 3-00:00:00
+#SBATCH --mail-type=all
+#SBATCH --job-name=avg_prcp
+#SBATCH --mail-user=kieranf@email.unc.edu
+#SBATCH --array=4-9
+
+# Determine raster processing unit (RPU) of interest based on task_id
+RPU="$(sed -n ${SLURM_ARRAY_TASK_ID}p /proj/characklab/projects/kieranf/flood_damage_index/analysis/CONUS_RPU_list.txt)"
+echo $RPU
+
+module purge
+module load anaconda
+export PYTHONWARNINGS="ignore"
+
+# Average gridded precipitation timeseries over catchments
+conda activate /proj/characklab/projects/kieranf/flood_damage_index/fli-env-v1
+python3.12 catchment_average_precip.py $RPU 2009 2024
+
+
