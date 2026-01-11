c9c6e3f95c3c9cccecc0d66c73f73313c8753542
Kieran Patrick Fitzmason (kieranf) <kpf10@pitt.edu>
2026-01-11 15:03:14 -0500

    renamed file to distinguish between upstream/overhead precip

--- rainfall_runoff/summarize_upstream_precip_by_event.sh
+++ rainfall_runoff/summarize_upstream_precip_by_event.sh
@@ -3,12 +3,12 @@
 #SBATCH -p general
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --mem=120g
+#SBATCH --mem=36g
 #SBATCH -t 3-00:00:00
 #SBATCH --mail-type=all
 #SBATCH --job-name=summarize_precip
 #SBATCH --mail-user=kieranf@email.unc.edu
-#SBATCH --array=42-106%20
+#SBATCH --array=1-41%20
 
 module purge
 module load anaconda
@@ -16,4 +16,4 @@ export PYTHONWARNINGS="ignore"
 
 # Summarize precipitation for each event
 conda activate /proj/characklab/projects/kieranf/flood_damage_index/fli-env-v1
-python3.12 summarize_precip_by_event.py
+python3.12 summarize_upstream_precip_by_event.py
